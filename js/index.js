b76f6695c29009a91ba5e51dea693093de86eb5d
Antoine <antoine.sb@orange.fr>
2018-04-14 15:39:09 +0100

    Creating addCircle() in class Map

--- js/index.js
+++ js/index.js
@@ -6,7 +6,9 @@ class Map {
 		this.circles = [];
 		this.polygons = [];
 		this.maximumZoom = 18;
-		// create actual map
+		this.posx = posx;
+		this.posy = posy;
+		// actual map creation
 		this.content = L.map(name).setView([posx, posy], zoom);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW50b2luZXNiIiwiYSI6ImNqZnpmdjZ4ZzBxcDczM213OTY0OW9vcnAifQ.K6l3pongKiYp0rzFvc4FtA', {
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -14,17 +16,26 @@ class Map {
 			id: 'mapbox.streets',
 			accessToken: 'pk.eyJ1IjoiYW50b2luZXNiIiwiYSI6ImNqZnpmdjZ4ZzBxcDczM213OTY0OW9vcnAifQ.K6l3pongKiYp0rzFvc4FtA'
 		}).addTo(this.content);
-		console.log("Map " + this.HTMLname + " has been created");
+		console.log("Map " + this.HTMLname + " has been created, centered to ");
 	}
-
 	addMarker(posx, posy) {
-		this.markers.push(L.marker([posx, posy]).addTo(this.content));
-		console.log("Marker added to map " + this.HTMLname + " at position" + [posx, posy]);
-		var marker = L.marker([51.5, -0.09]).addTo(this.content);
+		this.markers.push(
+			L.marker([posx, posy]).addTo(this.content)
+		);
+		console.log("Marker added to map " + this.HTMLname + " at position " + [posx, posy]);
+	}
+	addCircle(posx, posy, newColor, newFillColor, newFillOpacity, newRadius) {
+		this.circles.push(
+			L.circle([posx, posy], {
+				color: newColor,
+				fillColor: newFillColor,
+				fillOpacity: newFillOpacity,
+				radius: newRadius
+			}).addTo(this.content)
+		);
 	}
 };
 
-var myMap = new Map("map", 51.505, -0.09, 13);
-myMap.addMarker(51.5, -0.09);
-
-console.log("test");
+var myMap = new Map("map", 51.505, -.09, 13);
+myMap.addMarker(51.5, -.09);
+myMap.addCircle(51.508, -.11, 'red', '#f03', .5, 500);
