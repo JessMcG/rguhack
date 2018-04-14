08496a8a052f388fa5fd280eb2718b4af96c658d
Jess <plyadesdrunk@gmail.com>
2018-04-14 15:06:14 +0100

    Added map topology

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,8 +1,14 @@
 //Initialise Map & coordinates
 var mymap = L.map('mapid').setView([0, 0], 1);
 
-//Select map tiles
+//Select map tiles from server
 var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
 {attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ', maxZoom: 16 });
 
+//add map tiles to map
 Esri_WorldGrayCanvas.addTo(mymap);
+
+//add topological data to map
+var OpenTopoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+{maxZoom: 17, attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'});
+OpenTopoMap.addTo(mymap);
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+var express = require('express'); // Express web server framework
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
 const MongoClient = require('mongodb').MongoClient;
 const _ = require('lodash');
 const moment = require('moment');
+
+//using code from index.js to initialise database connection
+const client = await MongoClient.connect('mongodb://hackuser:hackuser@csdm-mongodb.rgu.ac.uk/hackais');
+const db = client.db('hackais');
+const cVessels = db.collection('vessels');
+const cPositions = db.collection('positions');
+const cTowns = db.collection('towns');
